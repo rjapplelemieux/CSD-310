e303ceacd0cfffe30269da26f95925714c1f1916
rjapplelemieux <rjapplelemieux@my365.bellevue.edu>
2023-11-10 13:20:35 -0600

    commit

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,4 +1,14 @@
--- Pytech C0llection List --
-['students']
+# import MongoClient
+import pymongo
+from pymongo import MongoClient
 
-End of program, press any key to exit...
+uri = "mongodb+srv://admin:admin@cluster0.gdme2al.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Create a variable named db and assign it to the pytech database instance
+db = client["pytech"]
+
+#print the list of collections int eh 'pytech' database
+print(db.list_collection_names)
