8688c805d3b0444a1f6a3bdec3a34f33e4b7e2b5
rjapplelemieux <rjapplelemieux@my365.bellevue.edu>
2023-11-18 14:13:31 -0600

    commit

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,12 +1,6 @@
-# import MongoClient
 import pymongo
 from pymongo import MongoClient
-
-# Create a new client and connect to the server
 client = MongoClient("mongodb+srv://admin:admin@cluster0.gdme2al.mongodb.net/?retryWrites=true&w=majority")
-
-# Create a variable named db and assign it to the pytech database instance
-db = client["pytech"]
-
-#print the list of collections int eh 'pytech' database
-print(db.list_collection_names)
+db = ["pytech"]
+collection = db["students"]
+print (db.list_collection_names)
