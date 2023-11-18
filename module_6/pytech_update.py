cb80a4831b06405d51d2d197ff81e2b862e7b1cb
rjapplelemieux <rjapplelemieux@my365.bellevue.edu>
2023-11-18 16:01:06 -0600

    commit

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -6,7 +6,14 @@ client = MongoClient("mongodb+srv://admin:admin@cluster0.gdme2al.mongodb.net/?re
 db = client["pytech"]
 collection = db["students"]
 
+post = {"_id": 1010, "first_name": "Jason", "last_name": "Pyke"}
+
 find_one = {"_id": 1007}
 print (find_one)
 
 update_one = {{"_id": 1007}, {"$set": {"last_name":"AppleLemieux"}}}
+update_one = {{"_id": 1007}, {"$set": {"_id":"1011"}}}
+
+collection.insert_one(post)
+
+result = db.collection.update_one({"_id": 1007}, {"$set": {"last_name": "AppleLemieux"}})
