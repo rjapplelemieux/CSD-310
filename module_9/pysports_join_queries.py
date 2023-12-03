8d3b9ab7c9f5cae96ec482ba1a2b65bb76c60b69
rjapplelemieux <rjapplelemieux@my365.bellevue.edu>
2023-12-03 15:57:58 -0600

    commit

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -1,20 +1,25 @@
 import mysql.connector
 from mysql.connector import Error
-connection = mysql.connector.connect(host="localhost",
-user="root2"
-passwd="root"
-db="pysports")
-try
-    if connection.is_connected():
-	    cursor = connection.cursor()
-	    cursor.execute ("select database();")
-	    db = cursor.fetchone()
-	    print("You're connected to database: ", db)
 
-except Error as e:
-	print("Error while connecting to MySQL", e)
-finally
-	if connection.is_connected():
-        cursor.close()
-        connection.close()
-	    print("MySQL connection is closed")
+connection = mysql.connector.connect(host="localhost",
+user="root",
+passwd="BlueDemon1234!@#",
+db="pysports")
+
+cursor = connection.cursor()
+
+sql = "INSERT INTO player (player_id, first_name, last_name) VALUES (%s, %s, %s)"
+val = [
+	("1", "Thorin", "Oakenshield"),
+	("2", "Bilbo", "Baggins"),
+	("3", "Frodo", "Baggins"),
+	("4", "Saruman", "The White"),
+	("5", "Angmar", "Witch-King"),
+	("6", "Azog", "The Defiler")
+]
+
+cursor.executemany(sql, val)
+
+connection.commit()
+
+print(cursor.rowcount, "was inserted.")
